ea5b005e69a587e3a0effb71f8b6a741cda921fc
Kyle <kasikay360@gmail.com>
2023-06-05 15:27:13 -0700

    NEW TO PYTHON

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 # first two letters goes here
-print(f"First 3 letters: {word[0:-6]}")
+print((f"First 3 letters: {word[0:-6]}").format(word=word))
 #last two letters goes here
-print(f"Last 2 letters: {word[7:]}")
+print((f"Last 2 letters: {word[7:]}").format(word=word))
 #midle word goes here
-print(f"Middle word: {word[1:-1]}")
+print((f"Middle word: {word[1:-1]}").format(word=word))
