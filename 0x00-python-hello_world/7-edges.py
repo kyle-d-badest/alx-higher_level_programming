9373a5fdf55d159352338729e9af8ce85d359b37
Kyle <kasikay360@gmail.com>
2023-06-05 12:15:11 -0700

    TESTING

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+# first two letters goes here
+print(f"First 3 letters: {word[0:-6]}")
+#last two letters goes here
+print(f"Last 2 letters: {word[7:]}")
+#midle word goes here
+print(f"Middle word: {word[1:-1]}")
